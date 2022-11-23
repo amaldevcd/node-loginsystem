64a32399823889a784afe17e27e7ddfaecd2c92e
AMAL <mailtoamaldevcd@gmail.com>
2022-11-23 22:57:41 +0530

    login system complete

---
+++ router.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var router = express.Router();
+
+const credential ={
+    email:"admin@gmail.com",
+    password:"admin123"
+}
+
+router.post('/login',(req,res)=>{
+    if(req.body.email==credential.email && req.body.password==credential.password)
+    {
+        req.session.user=req.body.email;
+        res.end("Successful login");
+    }
+    else{
+        res.end("Invalid username");
+    }
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,27 @@
 const express = require('express');
+const path = require('path');
+const bodyparser = require('body-parser');
+const session = require("express-session");
+const {v4:uuidv4} = require("uuid");
+
+const router = require('./router');
+
 const app = express();
 
 const port = process.env.PORT||3000;
 
 app.set('view engine','ejs');
+
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({extended:true}))
+
+app.use(session({
+    secret:uuidv4(),
+    resave:false,
+    saveUninitialized:true
+}))
+
+app.use('/route',router);
 
 app.get('/',(req,res)=>
 {
